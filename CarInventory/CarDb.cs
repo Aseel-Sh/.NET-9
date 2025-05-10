0ae35e2d33cea77a91ef642faa6d900cb6f6ed49
Aseel-Sh <aseel.sh52@gmail.com>
2025-05-10 11:29:52 -0500

    Finally added project files from CarInventory and VideoGameApi

---
+++ CarInventory/CarDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarInventory
+{
+    public class CarDb : DbContext
+    {
+        public CarDb(DbContextOptions<CarDb> options) : base(options) { }
+
+        //create cars table in db
+        public DbSet<Car> Cars { get; set; }
+
+    }
+}
---
+++ VideoGameApi/VideoGameApi/Controllers/VideoGameController.cs
@@ -0,0 +1,85 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Data;
+using VideoGameApi.Models;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
+    {
+        private readonly VideoGameDbContext _context = context;
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames() {
+           return Ok(await _context.VideoGames
+               .Include(x => x.VideoGameDetails)
+               .Include(x => x.Publisher)
+               .Include(x => x.Developer)
+               .Include(x => x.Genres)
+               .ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VideoGame>>GetVideoGameById (int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+            {
+                return BadRequest();
+            }
+
+            _context.VideoGames.Add(newGame);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVideoGame(int id, VideoGame updatedGame)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            game.Title = updatedGame.Title;
+            game.Publisher = updatedGame.Publisher;
+            game.Platform = updatedGame.Platform;
+            game.Developer = updatedGame.Developer;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVideGame(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            _context.VideoGames.Remove(game);
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
